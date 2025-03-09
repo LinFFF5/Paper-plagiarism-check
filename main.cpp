ef16062fc6e73cad471fb2b2b695a11ab20f6b1d
LinFFF5 <2545941332@qq.com>
2025-03-09 13:51:36 +0800

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "Paper-plagiarism-check.h"
 using namespace std;
-
 const char* const DICT_PATH = "dict/jieba.dict.utf8";
 const char* const HMM_PATH = "dict/hmm_model.utf8";
 const char* const USER_DICT_PATH = "dict/user.dict.utf8";
